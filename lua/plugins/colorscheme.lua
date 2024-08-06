f8fa8b28282954e04110ac1cbf5394fd2e6d5da3
Al <athila2001@gmail.com>
2024-08-06 13:30:52 +0700

    +copilotchat

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,13 +2,13 @@ return {
   -- add gruvbox
   -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   { "rebelot/kanagawa.nvim" },
-  -- { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
+  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
   -- { "Mofiqul/dracula.nvim" },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "gruvbox",
       -- "catppuccin",
       -- "gruvbox",
     },

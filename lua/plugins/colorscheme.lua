2ec31d37583ee387cf480a0314aa6e67dd8dc9e4
al <athila2001@gmail.com>
2025-02-07 16:25:05 +0700

    first init on omen

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,7 +2,19 @@ return {
   -- add gruvbox
   -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   { "rebelot/kanagawa.nvim" },
-  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    opts = {
+      italic = {
+        strings = false,
+        emphasis = false,
+        comments = true,
+        operators = false,
+        folds = false,
+      },
+    },
+  },
   -- { "Mofiqul/dracula.nvim" },
   -- Configure LazyVim to load gruvbox
   {
